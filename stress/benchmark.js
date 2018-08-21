52835053d468eeb6df8827774f61250911a37a16
unknown <jtlin91@gmail.com>
2018-08-21 11:59:47 -0700

    Stress test with siege and newrelic

--- stress/benchmark.js
+++ stress/benchmark.js
@@ -3,14 +3,14 @@ const siege = require('siege');
 const go = siege().on(3001);
 
 
-for (let i = 10000000; i < 12000000; i += 50) {
+for (let i = 11000000; i < 12000000; i += 500) {
   var hosts = go.get(`/api/about/hosts/${i}`).for(1).times;
   var reviews = go.get(`/api/about/reviews/${i}`).for(1).times;
   var hood = go.get(`/api/about/neighborhood/${i}`).for(1).times;
   var post = go.post('/api/about/post', { name: 'stresstest' }).for(1).times
 }
 
-hosts.concurrent(100).attack();
-reviews.concurrent(100).attack();
-hood.concurrent(100).attack();
-post.concurrent(100).attack();
+hosts.concurrent(50).attack();
+reviews.concurrent(50).attack();
+hood.concurrent(50).attack();
+post.concurrent(50).attack();
